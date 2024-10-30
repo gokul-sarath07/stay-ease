5c55923a76a12bd85bb40581fe48cd0589a62f89
Gokul Sarath <gokulsarath05@gmail.com>
2024-10-30 21:58:08 +0530

    BCryptPasswordEncoder bean created.

---
+++ src/main/java/com/crio/stayEase/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.crio.stayEase.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder getBCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
