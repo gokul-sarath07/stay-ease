c8c629f53d2a37a244a6092ddc6a4834fda97054
Gokul Sarath <gokulsarath05@gmail.com>
2024-10-31 20:52:12 +0530

    PathConstants classes created to store controller paths.

---
+++ src/main/java/com/crio/stayEase/config/PathConstants.java
@@ -0,0 +1,11 @@
+package com.crio.stayEase.config;
+
+public class PathConstants {
+    //  Base Paths
+    public static final String API_BASE_PATH = "/api/v1";
+    public static final String USER_BASE_PATH = API_BASE_PATH + "/auth";
+//    public static final String BOOK_BASE_PATH = API_BASE_PATH + "/books";
+
+    public static final String REGISTER_USER = USER_BASE_PATH + "/register";
+    public static final String LOGIN_USER = USER_BASE_PATH + "/login";
+}
